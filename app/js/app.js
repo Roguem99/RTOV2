5bb4722fecdeca89a4da98f3d3ea770b5b809dfa
Seth Tarrants <roguem99@live.com>
2017-05-06 19:36:29 -0400

    added import of datepicker-ionic2

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'App' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('App', ['ionic', 'ngCordova', 'ngAnimate'])
+angular.module('App', ['ionic', 'ngCordova', 'ngAnimate', 'datepicker-ionic2'])
 
 .run(['$ionicPlatform',
       function($ionicPlatform) {
